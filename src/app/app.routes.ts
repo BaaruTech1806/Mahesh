2a15fa37e1fdc5c8828843364e1d15aff04adb70
mahesh-del <maheshkusuma1999@gmail.com>
2024-09-03 16:31:05 +0530

    Added sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { ExpansionPanelComponent } from './components/expansion-panel/expansion-panel.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 export const routes: Routes = [
     {
-        path:"sidenav",component:ExpansionPanelComponent
+        path:"theory",component:ExpansionPanelComponent
+    },
+    {
+        path:"",component:LandingPageComponent
     }
 ];
