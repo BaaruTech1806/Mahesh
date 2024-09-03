15ec31a6210af9cf70aba02db09acc8a9ad3ecc6
mahesh-del <maheshkusuma1999@gmail.com>
2024-09-03 17:05:35 +0530

    Added padding for list items

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterLinkActive, RouterModule } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { SideNavComponent } from "../side-nav/side-nav.component";
 import { CommonModule } from '@angular/common';
@@ -11,7 +11,7 @@ import {MatListModule} from '@angular/material/list';
 @Component({
   selector: 'app-navbar', 
   standalone: true,
-  imports: [MatToolbarModule,MatListModule,RouterModule, MatButtonModule,CommonModule, MatIconModule, MatSidenavModule, SideNavComponent],
+  imports: [MatToolbarModule,MatListModule,RouterModule, MatButtonModule,CommonModule, MatIconModule, MatSidenavModule, SideNavComponent,RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
